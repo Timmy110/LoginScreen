5ffe73b478a6614ba584160ee497a5364c2bd9b2
Timmy110 <timothy_110@live.be>
2021-06-11 04:19:04 +0200

    Mapping login to JPA

--- src/main/java/com/example/LoginScreen/WebSecurityConfig.java
+++ src/main/java/com/example/LoginScreen/WebSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.LoginScreen;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,10 +16,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1").password(passwordEncoder().encode("user")).roles("USER");
+//        auth.inMemoryAuthentication()
+//                .withUser("user1").password(passwordEncoder().encode("user")).roles("USER");
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Bean
+    public CommandLineRunner initUser(UserRepository repository) {
+        return (args) -> {
+            repository.save(new User("user", passwordEncoder().encode("user"), "USER"));
+            repository.save(new User("user2", passwordEncoder().encode("user"), "USER" ));
+        };
     }
 
     @Bean
